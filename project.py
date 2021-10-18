e722591942ff6626f716acd521a2dd4dc7e5bfe4
Tumbo Lobi John <johnlobiit@gmail.com>
2021-10-18 10:35:01 +0300

    log transaction into the transaction file

--- project.py
+++ project.py
@@ -2,11 +2,19 @@ class Bank:
     def __init__(self, initial_amount=0 - 00):
         self.balance = initial_amount
 
+    def log_transaction(self, transaction_string):
+        with open("transaction.txt", "a") as file:
+            file.write(f"{transaction_string} \t\t\tBalance: {self.balance}\n")
+
     def withdrawal(self, amount):
         try:
             amount = float(amount)
         except ValueError:
             amount = 0
+        if amount:
+            self.balance = self.balance - amount
+            self.log_transaction(f"Withdrew Ksh.{amount}")
+
         self.balance = self.balance - amount
 
     def deposit(self, amount):
@@ -14,6 +22,9 @@ class Bank:
             amount = float(amount)
         except ValueError:
             amount = 0
+        if amount:
+            self.balance = self.balance - amount
+            self.log_transaction(f"Deposited Ksh.{amount}")
         self.balance = self.balance + amount
 
 
