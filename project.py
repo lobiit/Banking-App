121b7e661fad390fa86dbe5395299069443b4ae9
Tumbo Lobi John <johnlobiit@gmail.com>
2021-10-18 10:52:43 +0300

    allowed user input

--- project.py
+++ project.py
@@ -23,13 +23,30 @@ class Bank:
         except ValueError:
             amount = 0
         if amount:
-            self.balance = self.balance - amount
+            self.balance = self.balance + amount
             self.log_transaction(f"Deposited Ksh.{amount}")
         self.balance = self.balance + amount
 
 
 account = Bank(50.50)
-account.deposit(10)
-print(account.balance)
-account.withdrawal(14.75)
-print(account.balance)
+while True:
+    try:
+        action = input("What kind of action do you want to take?")
+    except KeyboardInterrupt:
+        print("\nLeaving the ATM\n")
+        break
+    if action in ["withdrawal", "deposit"]:
+        if action == "withdrawal":
+            amount = input("How much do you want to take out?")
+            account.withdrawal(amount)
+        else:
+            amount = input("How much do you want to put in?")
+            account.deposit(amount)
+
+        print("Your balance is", account.balance)
+
+    else:
+        print("That is not a valid action. Try again.")
+
+
+
